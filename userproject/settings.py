f83fc3cf35f378e8585e33229e418f636bc42620
Abhishek <abhishekganji27@gmail.com>
2022-07-16 16:12:43 +0530

    seventh commit

--- userproject/settings.py
+++ userproject/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["testdj2022.herokuapp.com", "localhost"]
+ALLOWED_HOSTS = ["testdj2022.herokuapp.com", "127.0.0.1"]
 
 
 # Application definition
@@ -128,6 +130,6 @@ STATICFILES_DIRS = [
     BASE_DIR / "static"
 ]
 
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
-CSRF_TRUSTED_ORIGINS = ["https://testdj2022.herokuapp.com/", "*"]
+CSRF_TRUSTED_ORIGINS = ["https://testdj2022.herokuapp.com","http://testdj2022.herokuapp.com","http://127.0.0.1"]
